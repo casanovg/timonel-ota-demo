2143688c86b44733c239c81b72a3d713cc89dc03
Gustavo Casanova <gustavo.casanova@nicebots.com>
2020-06-10 12:43:08 -0300

    Update timonel-twim-ota.cpp

--- timonel-twim-ota/src/timonel-twim-ota.cpp
+++ timonel-twim-ota/src/timonel-twim-ota.cpp
@@ -60,23 +60,31 @@ void CheckForUpdates(void) {
     const char ssid[] = SSID;
     const char password[] = PASS;
     const char host[] = "raw.githubusercontent.com";
-    const int port = 443;    
+    const int port = 443;
     // Reading update attempts recording file
     if (ReadFile(UPDATE_TRIES).charAt(0) != '\0') {
         int update_tries = (int)ReadFile(UPDATE_TRIES).charAt(0);
     }
     if (update_tries < MAX_UPDATE_TRIES) {
-        // Update retries NOT exceeded, starting update routine
+        // ..................................................
+        // (1) Update retries NOT exceeded, starting update routine
+        // ..................................................
         Serial.printf_P("[%s] Update attempts: %d or %d, starting the update routine ...\n\r", __func__, update_tries, MAX_UPDATE_TRIES);
         if (Exists(FW_LATEST_LOC)) {
-            // New firmware file already present in FS, probably due to a failed update
+            // ..................................................
+            // (?2) New firmware file already present in FS, probably due to a failed update
+            // ..................................................
             Serial.printf_P("[%s] New firmware file already present in FS, probably due to a failed update ...\n\r", __func__);
             fw_latest_dat = ReadFile(FW_LATEST_LOC);
         } else {
-            // New firmware file NOT present in FS, accessing the internet to check for updates
+            // ..................................................
+            // (:2) New firmware file NOT present in FS, accessing the internet to check for updates
+            // ..................................................
+            Serial.printf_P("[%s] New firmware file NOT present in FS, accessing the internet to check for updates ...\n\r", __func__);
+            // Check local firmware version, currently running on the slave device
             String fw_onboard_ver = ReadFile(FW_ONBOARD_VER);
             Serial.printf_P("[%s] ATtiny85 current firmware onboard: %s\n\r", __func__, fw_onboard_ver.c_str());
-
+            // " <<< Wifi connection "
             WiFi.mode(WIFI_STA);
             WiFi.begin(ssid, password);
             while (WiFi.status() != WL_CONNECTED) {
@@ -85,19 +93,26 @@ void CheckForUpdates(void) {
             }
             Serial.printf_P("\n\r");
             Serial.printf_P("WiFi connected! IP address: %s\n\r", WiFi.localIP().toString().c_str());
-            // Check the latest firmware version available for the slave device
+            // " WiFi connection >>> "
+            // Check the latest firmware version available for the slave device through WiFi
             char terminator = '\n';
-            //String url = "/casanovg/timonel-ota-demo/master/fw-attiny85/fw-latest.md";
             String fw_latest_ver = GetHttpDocument(FW_LATEST_VER, terminator, host, port, FINGERPRINT);
             //Serial.printf_P(".......................................................\n\r");
             Serial.printf_P("Latest firmware version available for ATtiny85: %s\n\r", fw_latest_ver.c_str());
+            if (fw_onboard_ver = fw_latest_ver) {
+                // (?3) Update NOT needed, run the application and exit this update routine
+                // ##### S #####
+            } else {
+                // (:3) There is a new firmware version available, download it through WiFi
+                
 
-
-        }
+            } // (/3)
+        } // (/2)
     } else {
-        // Update retries exceeded, running the application and exiting this update routine
-        // --------------------------------------------------------------------------------
-    }
+        // ..................................................
+        // (1) Update retries exceeded, running the application and exiting this update routine
+        // ..................................................
+    } // (/1)
 }
 
 #if POPEYE
